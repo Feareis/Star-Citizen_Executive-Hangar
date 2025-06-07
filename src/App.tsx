e18b2b84250cc466442592e4ffa3c67a567b621c
Fea <101436924+Feareis@users.noreply.github.com>
2025-06-07 23:08:42 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "@components/Layout";
 import Home from "@pages/Home";
+import Map from "@pages/Map";
+import ShipReward from "@pages/ShipReward";
 
 const App = () => {
   return (
+    // Main router wrapper
     <BrowserRouter>
+      {/* Global layout applied to all routes */}
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/map" element={<Map />} />
+          <Route path="/ship-reward" element={<ShipReward />} />
         </Routes>
       </Layout>
     </BrowserRouter>
