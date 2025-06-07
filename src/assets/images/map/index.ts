b75e376175f4e8a3c7cb93a479df38bf22b2340d
Fea <101436924+Feareis@users.noreply.github.com>
2025-06-07 23:09:40 +0200

    Update Images

---
+++ src/assets/images/map/index.ts
@@ -0,0 +1,13 @@
+import checkmate from "./cz_checkmate.webp";
+import orbituary from "./cz_orbituary.webp";
+import ruin_station from "./cz_ruin-station.webp";
+import pyam_supvisr from "./pyam-supvisr-3-4-5.webp";
+import pyam_exhang from "./pyam-exhang-0-1.webp";
+
+export const mapsAssets = {
+  CHECKMATE: checkmate,
+  ORBITUARY: orbituary,
+  RUINSTATION: ruin_station,
+  PYAMSUPVISR: pyam_supvisr,
+  PYAMEXHANG: pyam_exhang,
+};
---
+++ src/assets/images/ships/index.ts
@@ -0,0 +1,13 @@
+import f7amk2 from "./f7a-mk2.webp";
+import f8c from "./f8c.webp";
+import corsair from "./corsair.webp";
+import cutlass_black from "./cutlass-black.webp";
+import syulen from "./syulen.webp";
+
+export const shipsAssets = {
+  F7AMK2: f7amk2,
+  F8C: f8c,
+  CORSAIR: corsair,
+  CUTLASSBLACK: cutlass_black,
+  SYULEN: syulen,
+};
