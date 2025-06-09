9125b656c20a2ab04f2b1670e601f086445abd9f
Fea <101436924+Feareis@users.noreply.github.com>
2025-06-09 08:51:57 +0200

    fix ui

--- src/components/button/BuyMeACoffeeButton.tsx
+++ src/components/button/BuyMeACoffeeButton.tsx
@@ -4,7 +4,7 @@ export default function BuyMeACoffeeButton() {
       href="https://www.buymeacoffee.com/feareis"
       target="_blank"
       rel="noopener noreferrer"
-      className="fixed bottom-24 right-4 z-50 flex items-center gap-2 px-4 py-2 bg-[#FFDD00] hover:bg-[#ffce00] text-gray-800/80 rounded-lg shadow-xl transition-all duration-200 animate-float"
+      className="fixed bottom-24 right-4 z-50 flex items-center gap-2 px-4 py-2 bg-yellow-400 hover:bg-yellow-300/80 text-gray-800/80 rounded-lg shadow-xl transition-all duration-200 animate-float"
       style={{
         fontFamily: "'Comic Sans MS', 'Brush Script MT', cursive",
       }}
